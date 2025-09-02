df8951b71eb450bf2a071c1c438af2a6e2aa8e19
Dafa Falah <118614729+Dafalahl@users.noreply.github.com>
2025-09-02 19:58:30 +0700

    Update Mobil.java

--- praktikum-3/Mobil.java
+++ praktikum-3/Mobil.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.praktikum;
+package com.mycompany.praktikum-3;
 
 /**
  *
@@ -52,5 +52,6 @@ public class Mobil {
         System.out.println("Warna mobil berhasil diubah menjadi " + warnaBaru);
     }
 }
+
 
 
